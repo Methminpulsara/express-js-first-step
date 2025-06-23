74c3aaa9cccf7c573b9157483267540845543fb9
Methminpulsara <methminpulsara10@gmail.com>
2025-06-24 00:19:32 +0530

    update with post function

--- app.js
+++ app.js
@@ -4,11 +4,40 @@ const bodyParse = require('body-parser')
 const PORT =3000;
 
 const app = express();
+app.use(bodyParse.json())
+
+
+//annonymous funtion 
+
+// const get = function get(res, req){
+//     console.log('Hello World');
+//     res.send('Hello World');
+// } 
+
+// get kiyn function eka methnata pass krla thiyenw 
+// apita eeka annonimous widiyt krnn puluwn nisa kelinma meekt pass krnw 
+// app.get('/',(get)=>{
+//     res.send('Hello World');
+// })
+
+
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
 })
 
-app.pos
+app.post('/product/create/:id',(req,res)=>{
+    let body =  req.body;
+    let id= req.params.id;
 
-app.listen(3000)
+    console.log(body);
+    console.log(id);
+    res.send(body);
+
+})
+
+
+
+app.listen(3000,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
