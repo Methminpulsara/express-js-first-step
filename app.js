61a05a9e49d5b8e30369ae69b04a7ed621146d7e
Methminpulsara <methminpulsara10@gmail.com>
2025-06-23 23:15:21 +0530

    make server

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-(()=>{
-    console.log("Hello World");
-    // console.log(__dirname);
-    // console.log(__filename);
+const express = require('express');
+const bodyParse = require('body-parser')
+
+const PORT =3000;
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
 })
+
+app.pos
+
+app.listen(3000)
